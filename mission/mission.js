138077ad4e18b0de0b368a0c776c41c34350aa0d
Mayes007 <may23006@byui.edu>
2024-05-04 22:06:31 -0600

    main

--- mission/mission.js
+++ mission/mission.js
@@ -1,18 +1,2 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const themeSelector = document.querySelector("#theme-selector");
 
-    function changeTheme() {
-        const selectedTheme = themeSelector.value;
-        const logoImage = document.querySelector("footer img");
-
-        if (selectedTheme === "dark") {
-            document.body.classList.add("dark");
-            logoImage.src = "white_logo.png";
-        } else {
-            document.body.classList.remove("dark");
-            logoImage.src = "blue_logo.png";
-        }
-    }
-
-    themeSelector.addEventListener("change", changeTheme);
-});
+}
