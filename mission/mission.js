485e9d54a90f3cbfb52f7f382fd23d5c2c955fb7
Mayes007 <may23006@byui.edu>
2024-05-04 22:16:18 -0600

    main

--- mission/mission.js
+++ mission/mission.js
@@ -1,2 +1,24 @@
+const themeSelector = document.getElementById('themeSelector');
 
+// Function to change the theme
+function changeTheme() {
+    // Check the current value of the select element
+    const selectedTheme = themeSelector.value;
+
+    // If the selected theme is dark
+    if (selectedTheme === 'dark') {
+        // Add the dark class to the body
+        document.body.classList.add('dark');
+        // Change the logo image source to the white logo
+        document.querySelector('footer img').src = 'white_logo.png';
+    } else { // If the selected theme is light
+        // Remove the dark class from the body
+        document.body.classList.remove('dark');
+        // Change the logo image source back to the blue logo
+        document.querySelector('footer img').src = 'blue_logo.png';
+    }
 }
+
+// Adding event listener to the themeSelector element
+themeSelector.addEventListener('change', changeTheme);
+
