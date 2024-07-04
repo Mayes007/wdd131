4a112771ac8315f9f0e3ba89ef9325cd6872d7db
Mayes007 <may23006@byui.edu>
2024-07-03 20:13:21 -0600

    main

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,9 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const menuButton = document.querySelector('.menu-button');
-    const menu = document.querySelector('.menu');
+  const menuButton = document.querySelector('.menu-button');
+  const menu = document.querySelector('.menu');
 
-    menuButton.addEventListener('click', () => {
-        // Toggle the 'show' class to display/hide the menu
-        menu.classList.toggle('show');
-    });
+  menuButton.addEventListener('click', () => {
+      menu.classList.toggle('show');
+  });
+
+  function handleResize() {
+      if (window.innerWidth > 1000) {
+          menu.classList.remove('hide');
+      } else {
+          menu.classList.add('hide');
+      }
+  }
+
+  handleResize();
+  window.addEventListener('resize', handleResize);
 });
+function viewerTemplate(src, alt) {
+  return `
+      <div class="viewer">
+          <button class="close-viewer">X</button>
+          <img src="${src}" alt="${alt}">
+      </div>
+  `;
+}
+
+function viewHandler(event) {
+  const clickedElement = event.target;
+  if (clickedElement.tagName.toLowerCase() === 'img') {
+      const src = clickedElement.src.replace('-sm', '-full');
+      const alt = clickedElement.alt;
+      document.body.insertAdjacentHTML("afterbegin", viewerTemplate(src, alt));
+      document.querySelector(".close-viewer").addEventListener("click", closeViewer);
+  }
+}
+
+function closeViewer() {
+  document.querySelector(".viewer").remove();
+}
+
+document.querySelector(".gallery").addEventListener("click", viewHandler);
